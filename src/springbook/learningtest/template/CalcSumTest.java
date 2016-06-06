e8c4d2622256eb969cda1a8e22f1738f8e977a10
Hwang <hnaras@naver.com>
2016-06-07 00:26:21 +0900

    p258즈음..

--- src/springbook/learningtest/template/CalcSumTest.java
+++ src/springbook/learningtest/template/CalcSumTest.java
@@ -2,18 +2,36 @@ package springbook.learningtest.template;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class CalcSumTest {
-
+	
+	Calculator calculator;
+	String numFilepath;
+	
+	@Before
+	public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilepath = "src/springbook/learningtest/template/numbers.txt";
+	}
+	
 	@Test
 	public void sumOfNumbers() throws IOException {
-		Calculator calculator = new Calculator();
-		
-		
-		int sum = calculator.calcSum("D:/[workspace]/STS/SpringBook/bin/numbers.txt");
-		assertThat(sum, is(10));
+		assertThat(calculator.calcSum(this.numFilepath), is(10));
 	}
+	
+	@Test
+	public void multiplyOfNumbers() throws IOException {
+		assertThat(calculator.calMultiply(this.numFilepath), is(24));
+	}
+	
+	@Test
+	public void concatenateString() throws IOException {
+		assertThat(calculator.concatenate(this.numFilepath), is("1234"));
+	}
+	
+	
 }
--- src/springbook/learningtest/template/Calculator.java
+++ src/springbook/learningtest/template/Calculator.java
@@ -1,25 +1,32 @@
 package springbook.learningtest.template;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
 
 	public Integer calcSum(String filepath) throws IOException {
+		LineCallback<Integer> sumCallback = (line, value) -> value + Integer.valueOf(line);
+		return lineReadTemplate(filepath, sumCallback, 0);
+	}
+	
+	public Integer calMultiply(String filepath) throws IOException {
+		LineCallback<Integer> multiplyCallback = (line, value) -> value * Integer.valueOf(line);
+		return lineReadTemplate(filepath, multiplyCallback, 1);
+	}
+	
+	public String concatenate(String filepath) throws IOException {
+		LineCallback<String> concatenateCallback = (line, value) -> value + line;
+		return lineReadTemplate(filepath, concatenateCallback, "");
+	}
+	
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
 		BufferedReader br = null;
-		
 		try {
 			br = new BufferedReader(new FileReader(filepath));
-			Integer sum = 0;
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				sum += Integer.valueOf(line);
-			}
-			
-			br.close();
-			return sum;
+			int ret = callback.doSomethingWithReader(br);
+			return ret;
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			throw e;
@@ -32,6 +39,31 @@ public class Calculator {
 				}
 			}
 		}
+	}
+	
+	public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T intVal) throws IOException {
 		
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			T res = intVal;
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				res = callback.doSomethingWithLine(line, res);
+			}
+			return res;
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
 	}
 }
